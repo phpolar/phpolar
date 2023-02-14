4b9fa112a0902feb9a8052f37116c0235a703e57
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-02-14 07:12:10 -0600

    feat(RouteRegistry): add routing based on request methods
    Issue #68, Closes #68

--- tests/unit/Routing/RouteRegistryTest.php
+++ tests/unit/Routing/RouteRegistryTest.php
@@ -7,12 +7,13 @@ namespace Phpolar\Phpolar\Routing;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\MockObject\Stub\Stub;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(RouteRegistry::class)]
 final class RouteRegistryTest extends TestCase
 {
-    #[TestDox("Shall retrieve the request handlers associated with a route")]
+    #[TestDox("Shall retrieve the request handlers associated with a GET request")]
     public function test1()
     {
         $givenRoute = "/";
@@ -22,16 +23,61 @@ final class RouteRegistryTest extends TestCase
         $handlerSpy = $this->getMockForAbstractClass(AbstractRequestHandler::class);
         $handlerSpy->expects($this->once())->method("handle")->willReturn("");
         $sut = new RouteRegistry();
-        $sut->add($givenRoute, $handlerSpy);
-        $registeredHandler = $sut->get($givenRoute);
+        $sut->addGet($givenRoute, $handlerSpy);
+        $registeredHandler = $sut->fromGet($givenRoute);
+        $registeredHandler->handle();
+    }
+
+    #[TestDox("Shall retrieve the request handlers associated with a POST request")]
+    public function test2()
+    {
+        $givenRoute = "/";
+        /**
+         * @var MockObject&AbstractRequestHandler $handlerSpy
+         */
+        $handlerSpy = $this->getMockForAbstractClass(AbstractRequestHandler::class);
+        $handlerSpy->expects($this->once())->method("handle")->willReturn("");
+        $sut = new RouteRegistry();
+        $sut->addPost($givenRoute, $handlerSpy);
+        $registeredHandler = $sut->fromPost($givenRoute);
         $registeredHandler->handle();
     }
 
     #[TestDox("Shall return a RouteNotRegistered instance when a route is not associated with any handlers.")]
-    public function test2()
+    public function test3()
     {
         $sut = new RouteRegistry();
-        $result = $sut->get("an_unregistered_route");
+        $result1 = $sut->fromGet("an_unregistered_route");
+        $result2 = $sut->fromPost("an_unregistered_route");
+        $this->assertInstanceOf(RouteNotRegistered::class, $result1);
+        $this->assertInstanceOf(RouteNotRegistered::class, $result2);
+    }
+
+    #[TestDox("Shall not register a \"GET\" request in the \"POST\" request registry.")]
+    public function test4()
+    {
+        $givenRoute = "/";
+        /**
+         * @var Stub&AbstractRequestHandler $handlerSpy
+         */
+        $handlerSpy = $this->createStub(AbstractRequestHandler::class);
+        $sut = new RouteRegistry();
+        $sut->addGet($givenRoute, $handlerSpy);
+        $result = $sut->fromPost($givenRoute);
+        $this->assertInstanceOf(RouteNotRegistered::class, $result);
+    }
+
+    #[TestDox("Shall not register a \"POST\" request in the \"GET\" request registry.")]
+    public function test5()
+    {
+        $givenRoute = "/";
+        /**
+         * @var Stub&AbstractRequestHandler $handlerSpy
+         */
+        $handlerSpy = $this->createStub(AbstractRequestHandler::class);
+        $sut = new RouteRegistry();
+        $sut->addPost($givenRoute, $handlerSpy);
+        $result = $sut->fromGet($givenRoute);
         $this->assertInstanceOf(RouteNotRegistered::class, $result);
     }
 }
