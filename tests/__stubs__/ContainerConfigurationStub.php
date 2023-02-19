b822f679393b30a3020858d92d2aa1bf73f8ffee
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-02-19 05:29:09 -0600

    fix: respond to post requests
    Closes #89

--- tests/__stubs__/ContainerConfigurationStub.php
+++ tests/__stubs__/ContainerConfigurationStub.php
@@ -9,20 +9,38 @@ use ArrayAccess;
 final class ContainerConfigurationStub implements ArrayAccess
 {
     private array $values = [];
+    private array $raw = [];
+    private array $keys = [];
+    private array $frozen = [];
     public function offsetExists(mixed $offset): bool
     {
-        return isset($this->values[$offset]);
+        return isset($this->keys[$offset]);
     }
     public function offsetGet(mixed $offset): mixed
     {
-        return $this->values[$offset];
+        if (
+            isset($this->raw[$offset]) === true
+            || \is_object($this->values[$offset]) === false
+            || \method_exists($this->values[$offset], '__invoke') === false
+        ) {
+            return $this->values[$offset];
+        }
+
+        $raw = $this->values[$offset];
+        $val = $this->values[$offset] = $raw($this);
+        $this->raw[$offset] = $raw;
+
+        $this->frozen[$offset] = true;
+
+        return $val;
     }
     public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->values[$offset] = $value;
+        $this->keys[$offset] = true;
     }
     public function offsetUnset(mixed $offset): void
     {
-        unset($this->values[$offset]);
+        unset($this->values[$offset], $this->raw[$offset], $this->frozen[$offset]);
     }
 }
