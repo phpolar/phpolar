c48e0e67111b369c6a20dc40b278e71c59854b5b
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-02-20 05:54:19 -0600

    fix(AbstractModel): prevent type errors

--- src/Model/AbstractModel.php
+++ src/Model/AbstractModel.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Phpolar\Phpolar\Model;
 
+use ReflectionIntersectionType;
+use ReflectionNamedType;
 use ReflectionObject;
 use ReflectionProperty;
-
-;
+use TypeError;
 
 /**
  * Provides support for configuring the properties
@@ -34,7 +35,34 @@ abstract class AbstractModel
             $publicProps = (new ReflectionObject($this))->getProperties(ReflectionProperty::IS_PUBLIC);
             foreach ($publicProps as $prop) {
                 $propName = $prop->getName();
-                $this->$propName = (is_object($data) === true ? ($data->$propName ?? $this->$propName) : ($data[$propName] ?? $this->$propName));
+                $data = is_object($data) === false ? $data : get_object_vars($data);
+                if (isset($data[$propName]) === true) {
+                    $val = $data[$propName];
+                    if (gettype($val) === "string") {
+                        $type = $prop->getType();
+                        if ($type instanceof ReflectionIntersectionType) {
+                            throw new TypeError(
+                                "Cannot automatically set values when intersection types are declared.
+                                 Set the values manually."
+                            );
+                        }
+                        $casted = match (true) {
+                            $type instanceof ReflectionNamedType => match ($type->getName()) {
+                                "int" => (int) $val,
+                                "float" => (float) $val,
+                                "bool" => (bool) $val,
+                                "string" => $val,
+                                default => throw new TypeError(
+                                    "Cannot automatically set string source values to non-scalar
+                                     target properties.  Set the property manually."
+                                ), // @codeCoverageIgnore
+                            },
+                            default => $val,
+                        };
+                        $prop->setValue($this, $casted);
+                    }
+                    $prop->setValue($this, $val);
+                }
             }
         }
     }
