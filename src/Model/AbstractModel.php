4d370a32366a623ffeebd01c21ae1bee0592aba3
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-02-27 21:42:03 -0600

    test: remove code coverage exclusion annotations

---
+++ src/Model/AbstractModel.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpolar\Phpolar\Model;
+
+use ReflectionIntersectionType;
+use ReflectionNamedType;
+use ReflectionObject;
+use ReflectionProperty;
+use TypeError;
+
+/**
+ * Provides support for configuring the properties
+ * of an object for validation, formatting, and storage.
+ */
+abstract class AbstractModel
+{
+    use ValidationTrait;
+    use FieldErrorMessageTrait;
+    use LabelFormatTrait;
+    use ColumnNameTrait;
+    use DataTypeDetectionTrait;
+    use EntityNameConfigurationTrait;
+    use SizeConfigurationTrait;
+    use FormInputTypeDetectionTrait;
+    use FormControlTypeDetectionTrait;
+
+    /**
+     * @param array<string|int,mixed>|object $data
+     */
+    public function __construct(array | object $data = [])
+    {
+        if (empty($data) === false) {
+            $publicProps = (new ReflectionObject($this))->getProperties(ReflectionProperty::IS_PUBLIC);
+            foreach ($publicProps as $prop) {
+                $propName = $prop->getName();
+                $data = is_object($data) === false ? $data : get_object_vars($data);
+                if (isset($data[$propName]) === true) {
+                    $val = $data[$propName];
+                    if (gettype($val) === "string") {
+                        $type = $prop->getType();
+                        // @codeCoverageIgnoreStart
+                        if ($type instanceof ReflectionIntersectionType) {
+                            // Parser will catch
+                        }
+                        // @codeCoverageIgnoreEnd
+                        $casted = match (true) {
+                            $type instanceof ReflectionNamedType => match ($type->getName()) {
+                                "int" => (int) $val,
+                                "float" => (float) $val,
+                                "bool" => (bool) $val,
+                                "string" => $val,
+                                default => throw new TypeError(
+                                    "Cannot automatically set string source values to non-scalar
+                                     target properties.  Set the property manually."
+                                ),
+                            },
+                            default => $val,
+                        };
+                        $prop->setValue($this, $casted);
+                    }
+                    $prop->setValue($this, $val);
+                }
+            }
+        }
+    }
+}
