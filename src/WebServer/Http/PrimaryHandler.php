50794e272dfec29eee903cbab26ece95eb02bab0
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-02-27 21:49:29 -0600

    refactor: add primary handler

---
+++ src/WebServer/Http/PrimaryHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpolar\Phpolar\WebServer\Http;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * The HTTP Server request handling starting point
+ * for the application.
+ *
+ * This handler is implemented as a
+ * [queue-based handler](https://www.php-fig.org/psr/psr-15/meta/#queue-based-request-handler)
+ * that processes [PSR-15 middleware](https://www.php-fig.org/psr/psr-15/#22-psrhttpservermiddlewareinterface).
+ */
+class PrimaryHandler implements RequestHandlerInterface, MiddlewareQueueInterface
+{
+    /**
+     * @var MiddlewareInterface[]
+     */
+    private array $middlewareQueue = [];
+
+    public function __construct(private RequestHandlerInterface $fallbackHandler)
+    {
+    }
+
+    /**
+     * Gets the next middleware from the processing queue.
+     */
+    private function dequeue(): MiddlewareInterface
+    {
+        return array_shift($this->middlewareQueue);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if (count($this->middlewareQueue) === 0) {
+            return $this->fallbackHandler->handle($request);
+        }
+        $nextMiddleware = $this->dequeue();
+        return $nextMiddleware->process($request, $this);
+    }
+
+    public function queue(MiddlewareInterface $middleware): void
+    {
+        $this->middlewareQueue[] = $middleware;
+    }
+}
