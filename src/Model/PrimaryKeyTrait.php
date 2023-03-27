2dde10b1aad8fa306514d97fea6fb86681a7c48f
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-03-27 06:12:36 -0500

    fix: return declared types only
    Fixes build errors in CI.

--- src/Model/PrimaryKeyTrait.php
+++ src/Model/PrimaryKeyTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Phpolar\Phpolar\Model;
 
+use ReflectionNamedType;
 use ReflectionObject;
 use ReflectionProperty;
 
@@ -19,16 +20,34 @@ trait PrimaryKeyTrait
     {
         foreach ((new ReflectionObject($this))->getProperties(ReflectionProperty::IS_PUBLIC) as $publicProp) {
             if (count($publicProp->getAttributes(PrimaryKey::class)) !== 0) {
-                if ($publicProp->isInitialized($this) === true) {
-                    return $publicProp->getValue($this);
-                }
-                return match ($publicProp->getType()->getName()) {
-                    "string" => "",
-                    "int" => 0,
-                    default => null,
+                $propType = $publicProp->getType();
+                return match (true) {
+                    $propType instanceof ReflectionNamedType => match ($propType->getName()) {
+                        "string" => $this->getStringVal($publicProp),
+                        "int" => $this->getIntVal($publicProp),
+                        default => null,
+                    },
+                    default => match (true) {
+                        $publicProp->isInitialized($this) => match (gettype($publicProp->getValue($this))) {
+                            "string" => $this->getStringVal($publicProp),
+                            "integer" => $this->getIntVal($publicProp),
+                            default => null,
+                        },
+                        default => null,
+                    }
                 };
             }
         }
         return null;
+    }
+
+    private function getStringVal(ReflectionProperty $prop): string
+    {
+        return $prop->isInitialized($this) === true ? strval($prop->getValue($this)) : "";
+    }
+
+    private function getIntVal(ReflectionProperty $prop): int
+    {
+        return $prop->isInitialized($this) === true ? intval($prop->getValue($this)) : 0;
     }
 }
