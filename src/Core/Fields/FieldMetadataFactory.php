cde05d5b933333f3879debd4a69d12192b93f29b
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-09-02 21:20:19 -0500

    refactor: remove unused imports, use phpstan in ci
    Phan is not compatible with the pre-commit hook tool used.  I don't want suprises in ci.

--- src/Core/Fields/FieldMetadataFactory.php
+++ src/Core/Fields/FieldMetadataFactory.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Efortmeyer\Polar\Core\Fields;
 
-use Efortmeyer\Polar\Core\Attributes\AttributeCollection;
-
 final class FieldMetadataFactory
 {
     public function __construct(private FieldMetadata $field, private FieldMetadataConfig $config)
