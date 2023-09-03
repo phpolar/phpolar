109d52aeb2f15658438b11e49db00cca6df0b587
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-09-02 21:22:04 -0500

    feat(FieldErrorMessageTrait): add haserror method
    Issue #65, Closes #65

--- src/Model/FieldErrorMessageTrait.php
+++ src/Model/FieldErrorMessageTrait.php
@@ -47,19 +47,31 @@ trait FieldErrorMessageTrait
      */
     public function getFieldErrorMessage(string $fieldName, string $stringToAppend = ""): string
     {
-        if (
-            (new ReflectionProperty($this, "checked"))->isInitialized($this) === false ||
-                $this->checked === false
-        ) {
-            $this->checked = true;
-            $this->setErrorMessages();
-        }
-        $hasError = isset($this->errorMessages[$fieldName]);
-        return $hasError === true ? ($this->errorMessages[$fieldName] . $stringToAppend) : "";
+        $this->setErrorMsgsOnce();
+        return $this->hasError($fieldName) === true ? ($this->errorMessages[$fieldName] . $stringToAppend) : "";
     }
 
-    private function setErrorMessages(): void
+    /**
+     * Determines if a property is
+     * not valid.
+     *
+     * @api
+     */
+    public function hasError(string $fieldName): bool
     {
+        $this->setErrorMsgsOnce();
+        return isset($this->errorMessages[$fieldName]);
+    }
+
+    private function setErrorMsgsOnce(): void
+    {
+        if (
+            (new ReflectionProperty($this, "checked"))->isInitialized($this) === true &&
+                $this->checked === true
+        ) {
+            return;
+        }
+        $this->checked = true;
         $props = (new ReflectionObject($this))->getProperties(ReflectionProperty::IS_PUBLIC);
         array_walk(
             $props,
