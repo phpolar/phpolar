eb29809457cab5df0149163daede95a2285a9f8e
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-09-02 21:22:19 -0500

    fix(ContainerFactory): provide a container factory implementation
    Container factory implementations should be optional.

---
+++ src/WebServer/ContainerFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpolar\Phpolar\WebServer;
+
+/**
+ * A container factory implementation.
+ *
+ * Creates a PSR-11 container.
+ */
+final class ContainerFactory extends AbstractContainerFactory
+{
+}
