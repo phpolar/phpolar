a49e5ecab9a538a8637f673366834943840cffb7
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-06-29 17:47:53 -0400

    feat: add support for returning http statuses

---
+++ src/Http/Status/ClientError/Forbidden.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpolar\Phpolar\Http\Status\ClientError;
+
+/**
+ * Indicates that the origin server understood the request but refuses to authorize it.
+ *
+ * @link https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.3
+ */
+final class Forbidden
+{
+}
---
+++ src/Http/Status/ClientError/NotFound.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpolar\Phpolar\Http\Status\ClientError;
+
+/**
+ * Indicates that the origin server did not find a current representation
+ * for the target resource or is not willing to disclose that one exists.
+ *
+ * @link https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4
+ */
+final class NotFound
+{
+}
