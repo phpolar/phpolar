91263cf7778dea197257d7af475ead3e91e4d0d7
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-07-05 20:19:57 -0400

    test: exclude response builder from code coverage (#452)
    ResponseBuilder is deprecated and the unit tests have been deleted.
    
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- src/Http/ResponseBuilder.php
+++ src/Http/ResponseBuilder.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 
 /**
+ * @codeCoverageIgnore
  * @deprecated
  */
 final class ResponseBuilder implements ResponseBuilderInterface
